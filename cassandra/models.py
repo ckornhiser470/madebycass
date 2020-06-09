b984c16a05ca32f7b43f3e777051b1c69007b6f9
ckornhiser470 <cassandrakornhiser@Cassandras-MacBook-Air.local>
2020-06-09 16:07:01 -0400

    junenine

--- cassandra/models.py
+++ cassandra/models.py
@@ -3,13 +3,19 @@ class Art(models.Model):
     art_image=models.ImageField(upload_to='art_img', null=True)
     label=models.CharField(max_length=25, blank=True)
     description=models.TextField(max_length=500, blank=True)
+    year=models.IntegerField(null=True)
 
     def serialize(self):
         return{
          'id':self.id,
          'art_image':self.art_image.url,
-         'description':self.description
+         'description':self.description,
+         'label':self.label,
+         'year':self.year
         }
+    
+    class Meta:
+        ordering = ['-year']
 
     def __str__(self):
         return self.label
--- cassandra/static/cassandra/cass.js
+++ cassandra/static/cassandra/cass.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#nerd_view').style.display='none';
 
 const artImages=document.querySelectorAll('.art_img_btn');
 artImages.forEach(showArt);    
@@ -7,12 +8,21 @@ document.querySelector('#art').addEventListener('click', function(){
     document.querySelector('#art_view').style.display='block';
     document.querySelector('#who_view').style.display='none';
     document.querySelector('#sing_art_view').style.display='none';
+    document.querySelector('#nerd_view').style.display='none';
 
 })
 document.querySelector('#who').addEventListener('click', function(){
     document.querySelector('#art_view').style.display='none';
     document.querySelector('#who_view').style.display='block';
     document.querySelector('#sing_art_view').style.display='none';
+    document.querySelector('#nerd_view').style.display='none';
+
+})
+document.querySelector('#nerd').addEventListener('click', function(){
+    document.querySelector('#art_view').style.display='none';
+    document.querySelector('#who_view').style.display='none';
+    document.querySelector('#sing_art_view').style.display='none';
+    document.querySelector('#nerd_view').style.display='block';
 
 })
 
@@ -30,8 +40,9 @@ function showArt(artImages){
         fetch(`/art/${artID}`)
           .then(response => response.json())
           .then(artPiece =>{
-              const piece=`<div><img width='500' class="individual_art" src="${artPiece.art_image}"></img><p class="description">${artPiece.description}</p></div>`;
+              const piece=`<div class='indv'><h1 class='indv_label'>${artPiece.label}</h1><img width='500' class="individual_art" src="${artPiece.art_image}"></img><p class="description">${artPiece.description}</p></div>`;
               document.querySelector('#sing_art_view').innerHTML=piece;
+           
 
 })
 })

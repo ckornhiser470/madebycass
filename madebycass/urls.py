874017952380501d0e2731ee7756a1abaac15e1b
Cassandra Kornhiser <cvkornhiser@gmail.com>
2020-12-16 20:29:41 -0800

    Updates

--- madebycass/urls.py
+++ madebycass/urls.py
@@ -19,5 +19,4 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('cassandra.urls'))]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+    path('', include('cassandra.urls'))] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

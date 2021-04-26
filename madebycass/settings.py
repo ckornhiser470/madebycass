fdbda9ec421538162fe37085a5b90b3d119b2fda
Cassandra Kornhiser <cvkornhiser@gmail.com>
2021-04-26 10:29:13 -0400

    Fixed resposniveness of the about page

--- madebycass/settings.py
+++ madebycass/settings.py
@@ -135,11 +135,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'cassandra/static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+# STATIC_URL = '/static/'
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'cassandra/static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 print(STATIC_URL)
 print(STATIC_ROOT)
+print(STATICFILES_DIRS)
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
